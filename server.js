77ff7262dd6532e8d83980875e6050673054e455
JaiPremkumar <prem.jai411@gmail.com>
2024-12-24 16:26:36 +0530

    first new the  commitd

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const cors = require('cors')
 
 
  
-connectDatabase();    
+connectDatabase();     
 
 
-app.use(cors({
+/*app.use(cors({
     origin: ['https://cute-douhua-9ba391.netlify.app','http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'], // Add required headers
     credentials: true
-  })); 
+  })); */
+
+  app.use(cors("*"))
  
 //app.use(cors({ origin: '*' }));
 
